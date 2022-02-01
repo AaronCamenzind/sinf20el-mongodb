ec02a32f5bde0a259677fbe4430f1b1f01383066
Timo Sarkar <71646577+timosarkar@users.noreply.github.com>
2022-02-01 10:56:42 +0100

    Update mongoDBkennenlernen.rb

--- src/mongoDBkennenlernen.rb
+++ src/mongoDBkennenlernen.rb
@@ -2,3 +2,6 @@ require 'mongo'
 
 # Datenbank logging
 Mongo::Logger.logger.level = ::Logger::FATAL
+
+# Neue Datenbank erstellen und Verbinden
+client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'M141Test')
