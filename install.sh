ab1b62b23dee86a0d2dd5ab3805eaf6b90bacaa4
Timo Sarkar <71646577+timosarkar@users.noreply.github.com>
2022-02-08 11:11:45 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+ensure_netplan_apply() {
+    # First node up assign dhcp IP for eth1, not base on netplan yml
+    sleep 5
+    sudo netplan apply
+}
+
+step=1
+step() {
+    echo "Step $step $1"
+    step=$((step+1))
+}
+
+resolve_dns() {
+    step "===== Create symlink to /run/systemd/resolve/resolv.conf ====="
+    sudo rm /etc/resolv.conf
+    sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+}
+
+
+
+install_openssh() {
+    step "===== Installing openssh ====="
+    sudo apt update
+    sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    sudo apt install -y openssh-server
+    sudo systemctl enable ssh
+}
+
+install_tools() {
+    sudo apt install open-vm-tools-desktop gdebi ruby ruby-dev
+    gem install mongo
+    sudo gdebi frigg_0.4_all.deb
+}
+
+setup_root_login() {
+    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sudo systemctl restart ssh
+    sudo echo "root:rootroot" | chpasswd
+}
+
+
+main() {
+    ensure_netplan_apply
+    resolve_dns
+    install_openssh
+    setup_root_login
+    install_tools
+}
+
+main
