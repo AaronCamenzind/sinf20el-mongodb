a66fea3fa3ab839290e7a040129c927bc7de2f5d
AaronCamenzind <102143336+AaronCamenzind@users.noreply.github.com>
2022-03-22 10:23:33 +0100

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -1,7 +1,7 @@
 db.createCollection("Type", {
     validator: {
         bsonType: "object",
-        required: [ "TypeId" ],
+        required: [ "TypeId", "Name" ],
         properties: {
             Name: {
                 bsonType: "string",
@@ -18,19 +18,38 @@ db.createCollection("Type", {
         }
     }
 })
-
 db.createCollection("Systemuser", {
     validator: {
         bsonType: "object",
-        required: [ "userid" ],
+        required: [ "userid", "Name" ],
         properties: {
             Name: {
                 bsonType: "string",
-                maxlength: 50,
+                maxlenght: 50,
                 description: "Name Systemuser"
             },
 
-            UserID: {
+            userid: {
+                bsonType: "int",
+                unique: "true",
+                index: "true",
+                description: "Primary Key"
+            }
+        }
+    }
+})
+db.createCollection("usergroup", {
+    validator: {
+        bsonType: "object",
+        required: [ "groupid" ],
+        properties: {
+            Name: {
+                bsonType: "string",
+                maxlenght: 50,
+                description: "Group Name"
+            },
+
+            groupid: {
                 bsonType: "int",
                 unique: "true",
                 index: "true",
@@ -40,36 +59,123 @@ db.createCollection("Systemuser", {
     }
 })
 
-db.createCollection("Meta", {
+db.createCollection("Tag", {
     validator: {
         bsonType: "object",
-        required: [],
+        required: [ "tagid" ],
         properties: {
-            Path: {
+            Name: {
                 bsonType: "string",
-                maxlength: 100,
-                description: "Pfad"
+                maxlenght: 50,
+                description: "Tagname"
             },
-            
-            Perm: {
-                bsonType: "int",
-                description: "Perm",
-            },
-            
-            Time: {
-                bsonType: "timestamp",
-                description: "Aktuelle Zeit",
-            },
-            
-            Fn_UserId: {
-               bsonType: ObejctId( /* objectid of UserID of table Systemuser */ ) 
-            },
-            
-            MetaId: {
+
+            tagid: {
                 bsonType: "int",
                 unique: "true",
+                index: "true",
                 description: "Primary Key"
             }
+        }
+    }
+})
+db.createCollection("Data", {
+    validator: {
+        bsonType: "object",
+        required: [ "digest" ],
+        properties: {
+            digest: {
+                bsonType: "string",
+                maxlenght: 30,
+                unique: "true",
+                index: "true",
+                description: "Primary Key"
+            },
+              
+             content: {
+                bsonType: "string",
+                description: "content"
+            },
+             
+             size: {
+                bsonType: "int",
+                description: "groesse"
+            },
+             compression: {
+                bsonType: "int",
+                description: "Rate der Datenkompression"
+            },
+
+        }
+    }
+})
+
+db.createCollection("Import", {
+    validator: {
+        bsonType: "object",
+        properties: {
+            digest: {
+                bsonType: "string",
+                maxlenght: 35,
+                description: "Digest"
+            },
+
+            path: {
+                bsonType: "string",
+                description: "Path"
+            },
+
+             size: {
+                bsonType: "int",
+                description: "groesse"
+            },
+
+              Type: {
+                bsonType: "string",
+                description: "Datentyp"
+            },
+
+             mode: {
+                bsonType: "int",
+                description: "mode"
+            },
+
+             uid: {
+                bsonType: "int",
+                description: "UID"
+            },
+
+            user: {
+                bsonType: "string",
+                maxlenght: 50,
+                description: "Username"
+            },
+
+            gid: {
+                bsonType: "int",
+                description: "GID"
+            },
+
+            group: {
+                bsonType: "string",
+                description: "Gruppe"
+            },
+
+            Time: {
+                bsonType: "int",
+                maxlenght: 12,
+                description: "Zeit"
+            },
+
+            compression: {
+                bsonType: "int",
+                description: "compression"
+            },
+
+            Data: {
+                bsonType: "string",
+                description: "Data"
+            },
         }
     }
 })
